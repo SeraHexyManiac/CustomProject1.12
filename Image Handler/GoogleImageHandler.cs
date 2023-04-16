f2a5e35b1e2723fcdb7a4ea754185c8d24089cca
SeraHexyManiac <86704689+SeraHexyManiac@users.noreply.github.com>
2023-04-16 22:48:59 +0700

    Add files via upload

---
+++ Image Handler/GoogleImageHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CustomProject.Image_Handler
+{
+    public class GoogleImageHandler
+    {
+        public Dictionary<int, string> images = new Dictionary<int, string>();
+        public static readonly GoogleImageHandler Instance = new GoogleImageHandler();
+
+        static GoogleImageHandler() { }
+
+        public string GetImageAtId(int id)
+        {
+            var imageURL = images.TryGetValue(id, out var image);
+            return image;
+        }
+    }
+}
